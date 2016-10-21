078896051d58ae3fe01f60a97f6c20129300812d
James Haville <jameshaville@tenapp.co.uk>
2016-10-21 14:16:26 +0100

    specified api version to 2.2

--- fb.js
+++ fb.js
@@ -246,7 +246,7 @@
             }
 
             if(domain === 'graph') {
-                uri = 'https://graph.facebook.com/' + path;
+                uri = 'https://graph.facebook.com/v2.2/' + path;
                 isOAuthRequest = /^oauth.*/.test('oauth/');
             }
             else if(domain == 'api') {
